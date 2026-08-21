0274c380bff2f798996c1fb925aa26c15be59b82
starsFriday <starsFriday@users.noreply.github.com>
2026-08-21 22:00:41 +0800

    Fix quantized timestep embedding dtype

--- SenseNova/src/sensenova_u1/models/neo_unify/modeling_fm_modules.py
+++ SenseNova/src/sensenova_u1/models/neo_unify/modeling_fm_modules.py
@@ -56,7 +56,7 @@ class TimestepEmbedder(nn.Module):
 
     def forward(self, t):
         t_freq = self.timestep_embedding(t, self.frequency_embedding_size)
-        t_emb = self.mlp(t_freq.to(self.mlp[0].weight.dtype))
+        t_emb = self.mlp(t_freq.to(self.mlp[0].bias.dtype))
         return t_emb
 
 class ResBlock(nn.Module):
